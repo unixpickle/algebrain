dff926aff9c5e4208bbdf254198287782c59eb5a
Alex Nichol <unixpickle@gmail.com>
2016-11-06 13:49:15 -0500

    drafted SampleSet structure

---
+++ sample_set.go
@@ -0,0 +1,67 @@
+package algebrain
+
+import (
+	"github.com/unixpickle/num-analysis/linalg"
+	"github.com/unixpickle/sgd"
+	"github.com/unixpickle/weakai/rnn/seqtoseq"
+)
+
+// A SampleSet wraps a slice of Samples in an
+// sgd.SampleSet.
+type SampleSet []*Sample
+
+// Len returns the number of samples.
+func (s SampleSet) Len() int {
+	return len(s)
+}
+
+// Swap swaps two samples.
+func (s SampleSet) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+// GetSample produces a seqtoseq.Sample for the sample.
+func (s SampleSet) GetSample(i int) interface{} {
+	sample := s[i]
+
+	res := seqtoseq.Sample{}
+	for _, x := range sample.Query {
+		res.Inputs = append(res.Inputs, oneHotVector(x))
+		res.Outputs = append(res.Outputs, zeroVector())
+	}
+	res.Inputs = append(res.Inputs, oneHotVector(0))
+	res.Outputs = append(res.Outputs, zeroVector())
+
+	var last rune
+	for _, x := range sample.Response {
+		res.Inputs = append(res.Inputs, oneHotVector(last))
+		res.Outputs = append(res.Outputs, oneHotVector(x))
+		last = x
+	}
+
+	return res
+}
+
+// Copy returns a copy of the sample set.
+func (s SampleSet) Copy() sgd.SampleSet {
+	return append(SampleSet{}, s...)
+}
+
+// Subset returns a subset of the sample set.
+func (s SampleSet) Subset(i, j int) sgd.SampleSet {
+	return s[i:j]
+}
+
+func zeroVector() linalg.Vector {
+	return make(linalg.Vector, CharCount)
+}
+
+func oneHotVector(x rune) linalg.Vector {
+	res := make(linalg.Vector, CharCount)
+	ix := int(x)
+	if ix > 128 || ix < 0 {
+		panic("rune out of range: " + string(x))
+	}
+	res[ix] = 1
+	return res
+}
